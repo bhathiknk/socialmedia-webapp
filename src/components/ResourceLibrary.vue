35d0505b88fddae89cefc08f7cfb791820b886f7
Devnith Tissera <devnithtissera@gmail.com>
2023-09-30 20:02:39 +0530

    devnith

--- src/components/ResourceLibrary.vue
+++ src/components/ResourceLibrary.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="resourceLibrary">
-        <h1></h1>
+        <h1>Hello, its me i didi this to check whether the branching system is working!</h1>
     </div>
 </template>
 <script setup>
