35c84e96695cef3d9f4803fea208782dd170814f
sadisi <sadisi.rajapaksha218346@gmail.com>
2023-10-03 19:17:44 +0530

    EventCalender Bug Fixed added Live date

--- src/components/EventCalendar.vue
+++ src/components/EventCalendar.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h1>Calender</h1>
   </div>
+  
   <div id="Calender-frame">
     <div class="calendar-container">
       <v-calendar v-model="selectedDate" is-double-click="true" class="full-width-calendar" :attributes="attrs">
@@ -21,55 +22,6 @@
           <ul>
             <li v-for="(event, index) in events[selectedDate]" :key="index">{{ event }}</li>
           </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--/* After Coding is completed Delete This */-->
-  <div id="Edit-details" class="modal-dialog-scrollable">
-    <div class="alert alert-dismissible alert-warning">
-      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-      <h4 class="alert-heading">Warning!</h4>
-      <p class="mb-0">Developer Purpose only</p>
-    </div>
-
-    <div class="container">
-      <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-              <b>Template & Style (HTML & CSS) Changes</b>
-            </button>
-          </h2>
-          <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style="">
-            <div class="accordion-body">
-              <strong>1.</strong> Add a Calender Frame and Styles<code> #Calender-frame</code>, Dev: Vilochana Rajapaksaha.
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              <b>Script Changes</b>
-            </button>
-          </h2>
-          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <strong>1.</strong> Add a Live Date <code>:attributes="attrs"</code> Dev: Vilochana Rajapaksaha.
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              <b>Additional Notes</b>
-            </button>
-          </h2>
-          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <strong>1.</strong> Still Develping Calander body <code>Event saveing & Displaying ETC.. </code>, Dev: Vilochana Rajapaksaha.
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -106,16 +58,7 @@
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
 }
 
-/* After Coding is completed Delete This */
-#Edit-details{
-  max-width: 1000px;
-  margin: 0 auto;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 10px;
-  margin-top: 20px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-}
+
 </style>
 
 <script>
@@ -156,6 +99,7 @@ export default {
       {
         key: 'today',
         highlight: true,
+
         dates: new Date(),
       },
     ]);
