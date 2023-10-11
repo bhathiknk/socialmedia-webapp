623d420bee7fc1b766f0e4cba4df669cc81c26e4
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2023-10-11 23:19:11 +0530

    nav bar

--- src/components/DiscussionForum.vue
+++ src/components/DiscussionForum.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="goalSettings">
         <h1></h1>
+
+
+    <nav>
+      <ul>
+        <li><a href="Logo">LOGO</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="icon">Bell icon</a></li>
+      </ul>
+    </nav>
+
+
+
     </div>
 </template>
 <script setup>
+
+
+ 
+
+
 </script>
