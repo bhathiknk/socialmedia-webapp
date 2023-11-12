c8c95ef103469e11f90ef65f7f3b68f944c9b037
sadisi <sadisi.rajapaksha218346@gmail.com>
2023-11-12 22:41:35 +0530

    add grids to connection

--- src/views/Connection.vue
+++ src/views/Connection.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="feed">
-        <h1>This is an about Connection page</h1>
+        <h1>This is an about Connection page. Under maintenance by @Vilochana Dont do any changes..</h1>
+
+
+
+  <div class="all-body">
+      <div class="grid-container">
+        <div class="grid-item">Grid 1</div>
+        <div class="grid-item">Grid 2</div>
+        <div class="grid-item">Grid 3</div>
+      </div>
+  </div>
+
+
+
     </div>
 </template>
 
@@ -14,4 +27,32 @@ export default {
     }
 }
 </script>
+
+
+
+<style>
+
+
+.all-body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+}
+
+.grid-container {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  height: 100vh; /* Adjust this if needed */
+}
+
+.grid-item {
+  flex: 1;
+  text-align: center;
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+
+</style>
 
